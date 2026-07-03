130fbdbc60742e41d9d2353add687986e2f3d9f1
lipopsnb <lipopsnb@gmail.com>
2026-07-03 14:37:38 +0700

    Update warehouse.php

--- api/reports/warehouse.php
+++ api/reports/warehouse.php
@@ -4,7 +4,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/erp/config/auth.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/erp/config/functions.php';
 header('Content-Type: application/json; charset=utf-8');
 requireLogin();
-requireRole('director', 'accountant', 'manager', 'warehouse');
+requireRole('director');
 
 $pdo = getDBConnection();
 
